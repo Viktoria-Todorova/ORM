ecc6420cfd4cfeada742be9941ccf39a4b3a4eed
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-11-01 16:25:50 +0200

    8lab

--- 08-Advanced-Django-Model-Techniques-ex/orm_skeleton/settings.py
+++ 08-Advanced-Django-Model-Techniques-ex/orm_skeleton/settings.py
@@ -77,15 +77,21 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lab_8',
-        'USER': 'postgres',
-        'PASSWORD': '200697',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "",
+#         "USER": "",
+#         "PASSWORD": "",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- 08-Advanced-Django-Model-Techniques-lab/main_app/models.py
+++ 08-Advanced-Django-Model-Techniques-lab/main_app/models.py
@@ -1,3 +1,93 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
+from main_app.validator import validate_menu_categories
+
+
 # Create your models here.
+class ReviewMixin(models.Model):
+    reviewer_name = models.CharField(max_length=100)
+    rating = models.PositiveIntegerField(
+        validators=[MaxValueValidator(5)]
+    )
+    class Meta:
+        abstract = True
+
+
+class Restaurant(models.Model):
+    name = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(2,message ='Name must be at least 2 characters long.'),
+            MaxLengthValidator(100,message="Name cannot exceed 100 characters.")
+        ])
+
+    location = models.CharField(
+        max_length=200,
+        validators=[
+            MinLengthValidator(2,message="Location must be at least 2 characters long."),
+            MaxLengthValidator(100,message="Location cannot exceed 200 characters.")
+        ])
+    description = models.TextField(null=True,blank=True)
+    rating = models.DecimalField(max_digits=3,decimal_places=2,
+                                 validators=[
+                                     MinValueValidator(0,message="Rating must be at least 0.00."),
+                                     MaxValueValidator(5,message="Rating cannot exceed 5.00.")
+                                 ])
+
+#2
+class Menu(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    description =models.TextField(validators=[validate_menu_categories])
+    restaurant = models.ForeignKey(Restaurant,on_delete=models.CASCADE)
+
+#3
+# •	A default ordering in descending order for the model's database queries on the field "rating".
+# •	A human-readable name for the model in the admin interface - "Restaurant Review".
+# •	A human-readable plural name for the model in the admin interface - "Restaurant Reviews".
+# •	That NO two records for the fields "reviewer_name" and "restaurant" should share the same combination of values.
+class RestaurantReview(ReviewMixin):
+    # reviewer_name = models.CharField(max_length=100)
+    restaurant = models.ForeignKey(Restaurant,on_delete=models.CASCADE)
+    review_content = models.TextField()
+    # rating = models.PositiveIntegerField(
+    #     validators=[MaxValueValidator(5)]
+    # )
+
+    class Meta:
+        abstract = True #important for it to be inherited
+        ordering = ['-rating']
+        verbose_name='Restaurant Review'
+        verbose_name_plural='Restaurant Reviews'
+        unique_together = (('reviewer_name', 'restaurant'),)
+
+class RegularRestaurantReview(RestaurantReview):
+    pass
+
+class FoodCriticRestaurantReview(RestaurantReview):
+    food_critic_cuisine_area = models.CharField(max_length=100)
+    class Meta(RestaurantReview.Meta):
+
+        verbose_name = 'Food Critic Review'
+        verbose_name_plural = 'Food Critic Reviews'
+
+
+#5
+class MenuReview(ReviewMixin):
+    # reviewer_name = models.CharField(max_length=100)
+    menu = models.ForeignKey(Menu,on_delete=models.CASCADE)
+    review_content = models.TextField()
+    # rating = models.PositiveIntegerField(
+    #     validators=[MaxValueValidator(5)]
+    # )
+
+    class Meta:
+        ordering = ['-rating']
+        verbose_name = 'Menu Review'
+        verbose_name_plural = 'Menu Reviews'
+        unique_together = (('reviewer_name', 'menu'),)
+        indexes = [models.Index(fields=['menu'],name = 'main_app_menu_review_menu_id'),
+                 ]
+
