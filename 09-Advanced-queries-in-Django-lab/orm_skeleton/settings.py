c0be8f13b35db4f27e8d3f645e652d5ba923b6ee
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-11-02 14:28:40 +0200

    9

--- 09-Advanced-queries-in-Django-lab/orm_skeleton/settings.py
+++ 09-Advanced-queries-in-Django-lab/orm_skeleton/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
+#https://django-debug-toolbar.readthedocs.io/en/latest/installation.html
